a7617a854951f0d5e0e7fe98dc7cc598ff25e011
Milo Bedini <milobedini64@gmail.com>
2021-11-10 16:41:27 +0000

    secure route

--- config/router.js
+++ config/router.js
@@ -7,15 +7,22 @@ import {
   updateRecipe,
   deleteRecipe,
 } from "../controllers/recipes.js"
+import { loginUser, registerUser } from "../controllers/auth.js"
+import { getUserProfile } from "../controllers/users.js"
+import { secureRoute } from "./secureRoute.js"
 
 const router = express.Router()
 
-router.route("/recipes").get(getAllRecipes).post(addRecipe)
+router.route("/recipes").get(getAllRecipes).post(secureRoute, addRecipe)
 
 router
   .route("/recipes/:id")
   .get(getSingleRecipe)
-  .put(updateRecipe)
-  .delete(deleteRecipe)
+  .put(secureRoute, updateRecipe)
+  .delete(secureRoute, deleteRecipe)
+
+router.route("/register").post(registerUser)
+
+router.route("/login").post(loginUser)
 
 export default router
