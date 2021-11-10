42fd1d27fd753536efdc2c688c7a1d08078dfebe
Yin-wai Tse <yin-waitse@Gods-MacBook-Pro.local>
2021-11-10 15:58:57 +0000

    auth.js completed

--- config/environment.js
+++ config/environment.js
@@ -1,2 +1,3 @@
 export const port = 4000
 export const dbURI = "mongodb://127.0.0.1:27017/recipe-api"
+export const secret = "sdofisjdoifjsdnovnsopapow3e3242352f"
---
+++ controllers/users.js
@@ -0,0 +1,9 @@
+import User from '../models/user.js'
+
+export const getUserProfile = async (req, res) => {
+    try {
+        
+    } catch (err) {
+        console.log(err)
+    }
+}
