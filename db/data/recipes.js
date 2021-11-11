23f204f0fb730992d784ba48bb1cb94431f842e6
Yin-wai Tse <yin-waitse@Gods-MacBook-Pro.local>
2021-11-11 09:44:05 +0000

    recipes added

--- db/data/recipes.js
+++ db/data/recipes.js
@@ -10,5 +10,18 @@ export default [
     difficulty: 1,
     servings: 2,
     allergens: ["apples"]
+},
+
+{
+    name: "Chicken Curry Sauce",
+    image: "image url",
+    ingredients: ["2 chicken breasts", "2 tablespoons vegetable oil", "1 large onion", "2 cloves of garlic", "1 tablespoon curry powder", "300ml chicken stock", '1/2 tablespoon tomato puree', "1/2 teaspoon sugar", "2 tablespons Greek-style yogurt", "salt and pepper"],
+    categories: ["coriander"],
+    prepTime: 15,
+    cookTime: 75,
+    method: "Dice the chicken breast and season with salt and pepper, Heat the oil in a frying pan. Add the chicken brown it on all side for 6 minutes on medium heat. Remove and set aside on a plate. Cook the onions in the pan for 5 minutes until brown. Add the garlic and curry powder or paste and cook, stirring. for 1 minute. Add the stock, tomato puree, sugar and seasoning. Mix well and addd and the chicken. boil, then simmer for 45 minute or until tender. Remove the chicken to a serving dish using a draining spoon. heating the sauce over a high heat for 2-3 minutes until reduced and thickened",
+    difficulty: 2,
+    servings: 4,
+    allergens: [""]
 }
 ]
