380ea8e1500fbf5383406caedb31e76d6bdd76ae
Milo Bedini <milobedini64@gmail.com>
2021-11-11 16:51:19 +0000

    more LikedBy progress

--- config/router.js
+++ config/router.js
@@ -1,4 +1,4 @@
-import express from 'express'
+import express from "express"
 
 import {
   getAllRecipes,
@@ -9,33 +9,33 @@ import {
   addAComment,
   deleteAComment,
   addLikedBy,
-} from '../controllers/recipes.js'
-import { loginUser, registerUser } from '../controllers/auth.js'
-import { getUserProfile } from '../controllers/users.js'
-import { secureRoute } from './secureRoute.js'
+} from "../controllers/recipes.js"
+import { loginUser, registerUser } from "../controllers/auth.js"
+import { getUserProfile } from "../controllers/users.js"
+import { secureRoute } from "./secureRoute.js"
 
 const router = express.Router()
 
-router.route('/recipes').get(getAllRecipes).post(secureRoute, addRecipe)
+router.route("/recipes").get(getAllRecipes).post(secureRoute, addRecipe)
 
 router
-  .route('/recipes/:id')
+  .route("/recipes/:id")
   .get(getSingleRecipe)
   .put(secureRoute, updateRecipe)
   .delete(secureRoute, deleteRecipe)
 
-router.route('/recipes/:id/comments').post(secureRoute, addAComment)
+router.route("/recipes/:id/comments").post(secureRoute, addAComment)
 
 router
-  .route('/recipes/:id/comments/:commentId')
+  .route("/recipes/:id/comments/:commentId")
   .delete(secureRoute, deleteAComment)
 
-router.route('/recipe/:id/like').post(secureRoute, addLikedBy)
+router.route("/recipes/:id/like").post(secureRoute, addLikedBy)
 
-router.route('/register').post(registerUser)
+router.route("/register").post(registerUser)
 
-router.route('/login').post(loginUser)
+router.route("/login").post(loginUser)
 
-router.route('/profile').get(secureRoute, getUserProfile)
+router.route("/profile").get(secureRoute, getUserProfile)
 
 export default router
