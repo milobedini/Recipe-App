05315c4a84e8ed924931fda783f5cbda2978c09a
Milo Bedini <milobedini64@gmail.com>
2021-11-12 09:58:20 +0000

    isLoggedIn varoable

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,22 @@
-import './App.css'
-// import { useEffect } from 'react'
+import "./App.css"
+
+import { useEffect, useState } from "react"
+import { getToken } from "./helpers/auth"
 // import axios from 'axios'
-import RecipeList from './pages/RecipeList'
-import ShoppingListShow from './pages/ShoppingListShow'
+import RecipeList from "./pages/RecipeList"
+import ShoppingListShow from "./pages/ShoppingListShow"
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+  useEffect(() => {
+    if (getToken()) {
+      setIsLoggedIn(true)
+    } else {
+      setIsLoggedIn(false)
+    }
+  }, [])
+
   return (
     <>
       <h2> Recipes</h2>
