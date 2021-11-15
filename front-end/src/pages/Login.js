94baf00e8e4dca19fe3d1d488c32d7ba010e4218
Yin-wai Tse <yin-waitse@Gods-MacBook-Pro.local>
2021-11-15 09:53:48 +0000

    register and login complete

---
+++ front-end/src/pages/Login.js
@@ -0,0 +1,77 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { login } from '../helpers/api.js'
+import { setToken } from '../helpers/auth.js'
+import FormInput from '../components/FormInput'
+
+const Login = ({ setIsLoggedIn }) => {
+    const [data, setData] = useState({
+        email:'',
+        password:'',
+    })
+    const [errorInfo, setErrorInfo] = useState({})
+    const [isError, setIsError] = useState(false)
+    const navigate = useNavigate()
+
+    const handleSubmit = async (event) => {
+        event.preventDefault()
+
+        login(data).then(handleSuccessfulLogin).catch(handleError)
+    }
+
+    const handleSuccessfulLogin = ({ token }) => {
+        setToken(token)
+        setIsLoggedIn(true)
+        setIsError(false)
+        navigate.push('/recipes')
+    }
+
+    const handleError = (error) => {
+        if (error.response) {
+            setErrorInfo(error.response.data)
+            setIsError(true)
+        }
+    }
+
+    const handleFormChange = (event) => {
+        const { name, value } = event.target
+        setData({
+            ...data,
+            [name]: value,
+        })
+    }
+
+    const formInputProps = { data, errorInfo, handleFormChange }
+
+    return (
+        <section>
+            <form onSubmit={handleSubmit}>
+                <h1>Sign in to TasteBook</h1>
+                <FormInput 
+                    placeholder='email@email.com'
+                    type='email'
+                    name='email'
+                    {...formInputProps}
+                />
+                <FormInput
+                    placeholder='password'
+                    type='password'
+                    name='password'
+                    {...formInputProps}
+                />
+                <div>
+                    <input type='submit' value='login' />
+                </div>
+                {isError ? (
+                    <div className='error'>
+                        <p>Error. Please try again</p>
+                    </div>
+                ) : (
+                    <></>
+                )}
+            </form>
+        </section>
+    )
+}
+
+export default Login
--- front-end/src/pages/Register.js
+++ front-end/src/pages/Register.js
@@ -44,7 +44,41 @@ const Register = () => {
     return (
         <section>
             <form onSubmit={handleSubmit}>
-                
+            <h1>Register to TasteBook</h1>
+            <FormInput
+                placeholder='user123'
+                type='text'
+                name='username'
+                {...formInputProps} 
+            />
+            <FormInput
+                placeholder='email@email.com'
+                type='email'
+                name='email'
+                {...formInputProps}
+            />
+            <FormInput 
+                placeholder='password'
+                type='password'
+                name='password'
+                {...formInputProps}
+            />
+            <FormInput 
+                placeholder='password again'
+                type='password'
+                name='passwordConfirmation'
+                {...formInputProps}
+            />
+            <div>
+                <input type='submit' value='Register' />
+            </div>
+            {isError? (
+                <div className='error'>
+                    <p>Error. Please try again.</p>
+                </div>
+            ) : (
+                <></>
+            )}
             </form>
         </section>
     )
