af564c0e07f301ee1f3830cc8f6cb96750d58d33
Yin-wai Tse <yin-waitse@Gods-MacBook-Pro.local>
2021-11-18 12:08:20 +0000

    search

--- front-end/src/components/Search.js
+++ front-end/src/components/Search.js
@@ -2,8 +2,9 @@ import axios from 'axios'
 import { useState } from 'react'
 
 const Search = () => {
-    const[searchWord, setSearchWord] = useState('')
+    const[searchWord, setSearchWord] = useState([])
 
+    
     return (
         <div className='Search'>
             <input type='text' placeholder='Search...' onChange={event => {setSearchWord(event.target.value)}}/>
