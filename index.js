3a00b1ffaf77f260c4b0f48b60bace37d64c6972
Milo Bedini <milobedini64@gmail.com>
2021-11-24 18:06:29 +0000

    heroku configs

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import { port, dbURI } from './config/environment.js'
-import router from './config/router.js'
+import express from "express"
+import mongoose from "mongoose"
+import { port, dbURI } from "./config/environment.js"
+import router from "./config/router.js"
+import "dotenv/config"
+import path, { dirname } from "path"
+import { fileURLToPath } from "url"
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const app = express()
 
 const startServers = async () => {
   try {
     await mongoose.connect(dbURI)
-    console.log('Database has launched')
+    console.log("Database has launched")
 
     // JSON parser
     app.use(express.json())
@@ -19,17 +24,23 @@ const startServers = async () => {
       next()
     })
     // router
-    app.use('/api', router)
+    app.use("/api", router)
+
+    app.use(express.static(path.join(__dirname, "client", "build")))
+
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "client", "build", "index.html"))
+    })
 
     // catch all
     app.use((_req, res) => {
-      res.status(404).json({ message: 'Route Not Found' })
+      res.status(404).json({ message: "Route Not Found" })
     })
 
     // start the db once the server has been connected successfully
     app.listen(port, () => console.log(`Server up and running port ${port}`))
   } catch (err) {
-    console.log('There was an error! :(')
+    console.log("There was an error! :(")
     console.log(err)
   }
 }
